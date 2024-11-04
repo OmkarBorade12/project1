e85ea1045c3a7c1febe9235c512c2b9679d54868
Omkar Borade <139335700+OmkarBorade12@users.noreply.github.com>
2024-11-04 20:12:25 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+place = input("Place: ")
+adj1 = input("Adjective: ")
+animal = input("Animal: ")
+verb1 = input("Verb: ")
+
+verb2 = input("Verb: ")
+adj2 = input("Adjective: ")
+
+
+madlib1 = f"Today, I went to the {place} and saw a {adj1} {animal} that was {verb1}."
+madlib2 = f"I love to {verb2} videogames in my free time, but I'm not {adj2} in it."
+
+print(madlib1,madlib2)
